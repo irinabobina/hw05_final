f54a0fd31454908bf051e2f030910ddd519366f0
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-20 15:59:42 +0300

    jnk

--- posts/urls.py
+++ posts/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('new/', views.new_post, name='new_post'),
     path("follow/", views.follow_index, name="follow_index"),
     path("<str:username>/follow/", views.profile_follow, name="profile_follow"), 
+    path("<str:username>/unfollow/", views.profile_unfollow, name="profile_unfollow"),
     path('<username>/', views.profile, name='profile'),
     path('<username>/<int:post_id>/', views.post_view, name='post_detail'),
     path('<username>/<int:post_id>/edit', views.post_edit, name='post_edit'),
     path('<username>/<int:post_id>/comment', views.add_comment, name="add_comment"),
-    path("<str:username>/unfollow/", views.profile_unfollow, name="profile_unfollow"),
     path('400/', views.page_not_found, name='page_not_found'),
     path('500/', views.server_error, name='server_error')
 ]
