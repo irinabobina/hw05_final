4e82c56fd8fd2113e26f25672f4db4be8af68c9e
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-27 16:53:06 +0300

    kjnh

--- posts/urls.py
+++ posts/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("<str:username>/<int:post_id>/", views.post_view, name="post_detail"),
     path("<str:username>/<int:post_id>/edit/", views.post_edit, name="post_edit"),
     path("<str:username>/<int:post_id>/comment/", views.add_comment, name="add_comment"),
-    path("404/", views.page_not_found, name="page_not_found"),
+    path("404/", views.page_not_found, name="not_found"),
     path("500/", views.server_error, name="server_error")
 ]
