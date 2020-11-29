2e3086620edb0b93057343d66341ef1e95972f0d
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-29 22:19:33 +0300

    lkn kl

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-#from django.utils import timezone
 from django.views.decorators.cache import cache_page
 
 from .forms import PostForm, CommentForm
 from .models import Group, Post, User, Comment, Follow
+
 
 @cache_page(20)
 def index(request):
