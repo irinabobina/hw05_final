e6888f456ac3d51d0f9fc5e0cdf823e06311ec35
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-29 22:15:58 +0300

    ghj

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.utils import timezone
+#from django.utils import timezone
 from django.views.decorators.cache import cache_page
 
 from .forms import PostForm, CommentForm
